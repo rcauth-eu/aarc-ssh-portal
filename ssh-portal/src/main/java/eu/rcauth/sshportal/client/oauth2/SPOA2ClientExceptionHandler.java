2de810d7b508e42e33964e92e2580b3b04b42c17
Mischa Salle <msalle@nikhef.nl>
2019-04-15 22:43:16 +0200

    No longer need parseContent()
    We've fixed parseContent in OA4MP's OA2ClientExceptionHandler since we need it
    in all clients (and needed fixing for both plain and JSON anyway).

--- ssh-portal/src/main/java/eu/rcauth/sshportal/client/oauth2/SPOA2ClientExceptionHandler.java
+++ ssh-portal/src/main/java/eu/rcauth/sshportal/client/oauth2/SPOA2ClientExceptionHandler.java
@@ -1,7 +1,6 @@
 package eu.rcauth.sshportal.client.oauth2;
 
 import java.io.IOException;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,12 +9,10 @@ import javax.servlet.ServletException;
 
 import edu.uiuc.ncsa.myproxy.oa4mp.client.servlet.ClientServlet;
 import edu.uiuc.ncsa.oa4mp.oauth2.client.servlet.OA2ClientExceptionHandler;
-import edu.uiuc.ncsa.security.core.exceptions.GeneralException;
 import edu.uiuc.ncsa.security.servlet.ServiceClientHTTPException;
 import edu.uiuc.ncsa.security.core.util.MyLoggingFacade;
 
 import eu.rcauth.sshportal.servlet.SSHKeyMainServlet;
-import net.sf.json.JSONObject;
 
 /**
  * <p>Created by Mischa Sall&eacute;<br>
@@ -34,39 +31,12 @@ public class SPOA2ClientExceptionHandler extends OA2ClientExceptionHandler {
      * handles exceptions, including a {@link ServiceClientHTTPException} which is
      * thrown when an error is returned from the sshkeys endpoint. In that
      * case the content of the response is typically a JSON which is parsed
-     * by {@link #parseContent(String, HttpServletRequest)}
+     * by {@link OA2ClientExceptionHandler#parseContent(String, HttpServletRequest)}
      */
     @Override
     public void handleException(Throwable t, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        logger.warn("Handling exception t: "+t.getMessage());
-        // A ServiceClientHTTPException with status 400 is probably an error from the sshkeys servlet.
-        // The output is then a JSON with an error_description. It will be parsed by parseContent().
+        logger.warn("Handling exception: " + t.getMessage());
         super.handleException(t, request, response);
-    }
-
-    /**
-     * Parses the returned error response from the /sshkeys API endpoint.
-     * It expects a JSON and will set the key/value pairs as attribute in the request.
-     * @see OA2ClientExceptionHandler#handleException(Throwable, HttpServletRequest, HttpServletResponse)
-     */
-    @Override
-    protected void parseContent(String content, HttpServletRequest request) {
-        // Content is expected to be a JSON
-        boolean hasValidContent = false;
-        JSONObject errObject = JSONObject.fromObject(content);
-        // put the key/values in request, they will be used on the error page
-        Set hset = errObject.keySet();
-        for (Object key : hset) {
-            if (key instanceof String) {
-                hasValidContent = true; // we manage to parse at least one key
-                request.setAttribute((String)key, errObject.get(key));
-            }
-        }
-
-        if (!hasValidContent) {
-            logger.warn("Cannot parse body or error");
-            throw new GeneralException();
-        }
     }
 
 }
