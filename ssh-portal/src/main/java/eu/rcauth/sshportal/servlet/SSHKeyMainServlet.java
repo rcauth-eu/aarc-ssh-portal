bb624425ef493f41c05fae95aff55dff0200a623
Mischa Salle <msalle@nikhef.nl>
2026-05-06 12:18:29 +0200

    Replace discontinued net.sf.json-lib:json-lib
    net.sf.json-lib:json-lib is discontinued, move to org.kordamp.json:json-lib-core
    which is more or less a drop-in replacement.

--- ssh-portal/src/main/java/eu/rcauth/sshportal/servlet/SSHKeyMainServlet.java
+++ ssh-portal/src/main/java/eu/rcauth/sshportal/servlet/SSHKeyMainServlet.java
@@ -29,10 +29,10 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.io.IOUtils;
 
-import net.sf.json.JSON;
-import net.sf.json.JSONSerializer;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
+import org.kordamp.json.JSON;
+import org.kordamp.json.JSONSerializer;
+import org.kordamp.json.JSONArray;
+import org.kordamp.json.JSONObject;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
